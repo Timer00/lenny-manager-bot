a5ed94144bd6f146e33fb60af6689530af05f1b4
Timer00 <theocarraraleao@gmail.com>
2017-10-02 21:00:45 -0300

    token

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const bot = require("./bot-client");
 const moment = require('moment');
-//const token = process.env["LENNY_TOKEN"];
-const token = require("./secret.json").token;
+const token = process.env["LENNY_TOKEN"];
+//const token = require("./secret.json").token;
 const MemberInfo = require("./member-info");
 const state = require("./state");
 const {parseTime, unindent, toLowerInitial} = require("./util");
